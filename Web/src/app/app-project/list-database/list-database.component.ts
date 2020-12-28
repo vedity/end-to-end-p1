9af1f953d7921a5d40275318f70de1d47366c29a
kmpatel <karishma.patel@atozinfoway.com>
2020-12-28 19:26:56 +0530

    service changes

--- Web/src/app/app-project/list-database/list-database.component.ts
+++ Web/src/app/app-project/list-database/list-database.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataTableDirective } from 'angular-datatables';
-
+import { ApiService } from '../api.service';
 @Component({
     selector: 'app-list-database',
     templateUrl: './list-database.component.html',
@@ -11,51 +11,13 @@ export class ListDatabaseComponent implements OnInit {
     datatableElement: DataTableDirective;
     dtOptions: DataTables.Settings = {};
     filter:boolean=true;
-    constructor() { }
+    constructor(public apiService:ApiService) { }
     transactions: any;
     ngOnInit(): void {
-        this.transactions = [
-            {
-                "dataset_id": 1,
-                "dataset_name": "Pima Indians diabetes Dataset",
-                "file_name": "pima_indians_diabetes_2020_12_22_09_04_23.csv",
-                "file_size": "24.05 kb",
-                "dataset_table_name": "di_pima_indians_diabetes_2020_12_22_09_04_23_tbl",
-                "dataset_visibility": "public",
-                "user_name": "Neha",
-                "created_on": "2020-12-22T09:04:23.921Z"
-            },
-            {
-                "dataset_id": 2,
-                "dataset_name": "Iris Dataset",
-                "file_name": "iris_2020_12_22_09_05_20.csv",
-                "file_size": "4.7 kb",
-                "dataset_table_name": "di_iris_2020_12_22_09_05_20_tbl",
-                "dataset_visibility": "public",
-                "user_name": "Neha",
-                "created_on": "2020-12-22T09:05:20.355Z"
-            },
-            {
-                "dataset_id": 3,
-                "dataset_name": "CarPrice Assignment Dataset",
-                "file_name": "CarPrice_Assignment_2020_12_22_09_06_24.csv",
-                "file_size": "26.72 kb",
-                "dataset_table_name": "di_carprice_assignment_2020_12_22_09_06_24_tbl",
-                "dataset_visibility": "public",
-                "user_name": "Rahul",
-                "created_on": "2020-12-22T09:06:24.169Z",
-            },
-            {
-                "dataset_id": 4,
-                "dataset_name": "CC General Dataset",
-                "file_name": "CC_GENERAL_2020_12_22_09_08_34.csv",
-                "file_size": "0.9 Mb",
-                "dataset_table_name": "di_cc_general_2020_12_22_09_08_34_tbl",
-                "dataset_visibility": "public",
-                "user_name": "Ketan",
-                "created_on": "2020-12-22T09:08:34.307Z"
-            }
-        ];
+      this.apiService.getDataset().subscribe(
+        logs =>{ this.transactions=logs.Data},
+        error => console.log(error)
+      );
     }
 
     displayfilter(){
--- Web/src/app/app-project/list-project/list-project.component.ts
+++ Web/src/app/app-project/list-project/list-project.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { DataTableDirective } from 'angular-datatables';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-list-project',
@@ -11,68 +12,26 @@ import { DataTableDirective } from 'angular-datatables';
 })
 export class ListProjectComponent implements OnInit {
 
-  @ViewChild(DataTableDirective, {static: false})
+  @ViewChild(DataTableDirective, { static: false })
   datatableElement: DataTableDirective;
-  dtOptions: DataTables.Settings = {};
-  filter:boolean=true;
-  constructor(public router:Router,public http:HttpClient) { }
+  dtOptions: DataTables.Settings = {
+    
+  };
+  filter: boolean = true;
+  constructor(public router: Router, public http: HttpClient, public apiService: ApiService) { }
   transactions: any;
   ngOnInit(): void {
-    this.transactions = [
-      {
-          "project_id": 1,
-          "project_name":"Pima Indians diabetes Project",
-          "project_desc": "Pima Indians diabetes Data",
-          "dataset_status" :0,
-          "model_status": 1,
-          "deployment_status":  1,
-          "user_name":"Neha",
-          "dataset_id":  1,
-          "created_on": "2020-12-22T09:04:23.923Z",
-      },
-      {
-          "project_id":  2,
-          "project_name": "Iris Data Project",
-          "project_desc": "Iris Data ",
-          "dataset_status":0,
-          "model_status":  1,
-          "deployment_status": 1,
-          "user_name":  "Neha",
-          "dataset_id":  2,
-          "created_on":  "2020-12-22T09:05:20.359Z"
-      },
-      {
-          "project_id": 3,
-          "project_name": "CarPrice Assignment Project",
-          "project_desc":  "CarPrice Assignment Data ",
-          "dataset_status":  0,
-          "model_status":1,
-          "deployment_status":  1,
-          "user_name":"Rahul",
-          "dataset_id":  3,
-          "created_on":"2020-12-22T09:06:24.172Z"
-      },
-      {
-          "project_id": 4,
-          "project_name": "CC General Project",
-          "project_desc": "CC General  Data ",
-          "dataset_status":  0,
-          "model_status": 1,
-          "deployment_status":1,
-          "user_name": "Ketan",
-          "dataset_id":  4,
-          "created_on": "2020-12-22T09:08:34.310Z"
-      }
-  ];
-}
-
-  
+    this.apiService.getproject().subscribe(
+      logs => { this.transactions = logs.Data },
+      error => console.log(error)
+    );
+  }
 
   ngAfterViewInit(): void {
     this.datatableElement.dtInstance.then((dtInstance: DataTables.Api) => {
       dtInstance.columns().every(function () {
         const that = this;
-        $('input',this.header()).on('keyup change', function () {
+        $('input', this.header()).on('keyup change', function () {
           if (that.search() !== this['value']) {
             that
               .search(this['value'])
@@ -83,17 +42,15 @@ export class ListProjectComponent implements OnInit {
     });
   }
 
-  displayfilter(){
-    this.filter=!this.filter;
+  displayfilter() {
+    this.filter = !this.filter;
     console.log(this.filter);
     $('.filter').val('').trigger('change');
-   // elem.value += ' NEW';
   }
 
-  create(){
+  create() {
     this.router.navigate(['create']);
   }
-
 
   getUserNumber(): Observable<any> {
     return this.http.get<any>("http://127.0.0.1:8000/mlaas/ingest/create_project");
