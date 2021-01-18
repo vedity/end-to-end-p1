f62c7d1985efc863d9cb5269bcd898d111d564ba
Mansi Kanjaria <mkanjaria@infosenseglobal.com>
2021-01-18 17:36:03 +0530

    login issue resolved

--- MS/mlaas/common/urls.py
+++ MS/mlaas/common/urls.py
@@ -4,9 +4,9 @@ from .views import *
 urlpatterns = [
     
     #URL For User Login
-    path('mlaas/ingest/user/login/',UserLoginClass.as_view()),
+    path('mlaas/ingest/user/login',UserLoginClass.as_view()),
 
     #URL for menu
-    path('mlaas/menu/',MenuClass.as_view()),
+    path('mlaas/menu',MenuClass.as_view()),
 
 ]
