f15148e2180ed633f7136530542ebedb141ee17a
kmpatel2019 <karishma.patel@infosenseglobal.com>
2021-02-01 18:50:50 +0530

    code cleanup - datatable issue - set least - most value

--- Web/src/app/app-project/app-project-routing.module.ts
+++ Web/src/app/app-project/app-project-routing.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LayoutComponent } from '../layouts/layout.component';
 import { ListProjectComponent } from './list-project/list-project.component';
 import { ListDatabaseComponent } from './list-database/list-database.component';
 import { ListDatadetailComponent } from './list-datadetail/list-datadetail.component';
 import { ManageProjectComponent } from './manage-project/manage-project.component';
 import { DetailviewProjectComponent } from './detailview-project/detailview-project.component';
 import { CreateProjectComponent } from './create-project/create-project.component';
-import { CreateSchemaMappingComponent } from './create-schema-mapping/create-schema-mapping.component';
 import { ManageSchemaMappingComponent } from './manage-schema-mapping/manage-schema-mapping.component';
-
-// import { AdvancedSortableDirective } from '../shared/widget/transaction/advanced-sortable.directive';
-//import { CyptolandingComponent } from './cyptolanding/cyptolanding.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'project' },
@@ -22,13 +17,10 @@ const routes: Routes = [
   {path: 'upload', component: DetailviewProjectComponent},
   {path: 'create', component: CreateProjectComponent},
   {path: 'schema', component: ManageSchemaMappingComponent},
-
 ];
-
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 
 export class AppProjectRoutingModule { }
-// canActivate: [AuthGuard]
--- Web/src/app/app-project/schema-mapping-api.service.ts
+++ Web/src/app/app-project/schema-mapping-api.service.ts
@@ -1,49 +1,42 @@
 import { Injectable } from '@angular/core';
-import {HttpClient,HttpHeaders, HttpParams } from '@angular/common/http';
-import  { Observable } from 'rxjs';
-
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class SchemaMappingApiService {
+  baseUrl = 'http://127.0.0.1:8000/mlaas/'
+  headers = new HttpHeaders({
+    'Content-type': 'application/json',
+  });
+  user: any;
+  constructor(private httpClient: HttpClient) { }
 
-   baseUrl = 'http://127.0.0.1:8000/mlaas/'
-   headers = new HttpHeaders ({
-     'Content-type': 'application/json',
-   });
-   user:any;
-   
-  constructor( private httpClient : HttpClient) { 
-
-  }
-  
-  getDataDetails(obj,dataset_id):Observable<any>{
-    var params=new HttpParams().append("dataset_id",dataset_id)
-    return this.httpClient.post(this.baseUrl+"ingest/data_detail/",obj,{headers:this.headers,params});
+  getDataDetails(obj, dataset_id): Observable<any> {
+    var params = new HttpParams().append("dataset_id", dataset_id)
+    return this.httpClient.post(this.baseUrl + "ingest/data_detail/", obj, { headers: this.headers, params });
   }
 
- 
-  getColumnList(dataset_id):Observable<any>{
-    var params=new HttpParams().append("dataset_id",dataset_id)
-    return this.httpClient.get(this.baseUrl+"ingest/data_detail/column_list/",{headers:this.headers,params});
+  getColumnList(dataset_id): Observable<any> {
+    var params = new HttpParams().append("dataset_id", dataset_id)
+    return this.httpClient.get(this.baseUrl + "ingest/data_detail/column_list/", { headers: this.headers, params });
   }
 
-  getColumnAttributes():Observable<any>{
-    return this.httpClient.get(this.baseUrl+"dataset_schema/column_attribute_list/",{headers:this.headers});
+  getColumnAttributes(): Observable<any> {
+    return this.httpClient.get(this.baseUrl + "dataset_schema/column_attribute_list/", { headers: this.headers });
   }
 
-  getDatatypeList():Observable<any>{
-    return this.httpClient.get(this.baseUrl+"dataset_schema/datatype_list/",{headers:this.headers});
+  getDatatypeList(): Observable<any> {
+    return this.httpClient.get(this.baseUrl + "dataset_schema/datatype_list/", { headers: this.headers });
   }
 
-  getDatasetSchema(dataset_id):Observable<any>{
-    var params=new HttpParams().append("project_id",dataset_id)
-    return this.httpClient.get(this.baseUrl+"ingest/dataset_schema/",{headers:this.headers,params});
+  getDatasetSchema(dataset_id): Observable<any> {
+    var params = new HttpParams().append("project_id", dataset_id)
+    return this.httpClient.get(this.baseUrl + "ingest/dataset_schema/", { headers: this.headers, params });
   }
-  
-  saveDatasetSchema(dataset_id,project_id,obj):Observable<any>{
-    var params=new HttpParams().append("dataset_id",dataset_id).append("project_id",project_id)
-    return this.httpClient.post(this.baseUrl+"ingest/dataset_schema/",obj,{headers:this.headers,params});
+
+  saveDatasetSchema(dataset_id, project_id, obj): Observable<any> {
+    var params = new HttpParams().append("dataset_id", dataset_id).append("project_id", project_id)
+    return this.httpClient.post(this.baseUrl + "ingest/dataset_schema/", obj, { headers: this.headers, params });
   }
 }
-
