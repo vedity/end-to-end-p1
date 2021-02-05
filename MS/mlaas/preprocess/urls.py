89825670a9e4a2e1c355689080015da94da1dd13
abhishek <abhishekn@infosenseglobal.net>
2021-02-05 15:08:51 +0530

    final commit

--- MS/mlaas/preprocess/urls.py
+++ MS/mlaas/preprocess/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     #URL for Dataset Statistics
     path('mlaas/preprocess/exploredata/get_data_statistics',DatasetExplorationClass.as_view()),
 
-    #URL for Data Visualization
-    path('mlaas/preprocess/visualize/get_visualization',DataVisualizationClass.as_view()),
+    # #URL for Data Visualization
+    # path('mlaas/preprocess/visualize/get_visualization',DataVisualizationClass.as_view()),
 
     #URL For dataset Schema
     path('mlaas/ingest/dataset_schema/',SchemaClass.as_view()),
