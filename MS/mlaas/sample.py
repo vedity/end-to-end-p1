35fc16efc2672843aaa6cb27e5e1dc7dd9ee1413
= <=>
2021-02-12 13:12:07 +0530

    minor changes

--- MS/mlaas/sample.py
+++ MS/mlaas/sample.py
@@ -12,31 +12,40 @@ driver=webdriver.Firefox()
 driver.maximize_window()
 
 #navigate to the url  
-driver.get("localhost:4200/")  
-#identify the Google search text box and enter the value 
-driver.find_element_by_id("email").clear()
-driver.find_element_by_id("email").send_keys("nisha")
+driver.get("localhost:4200/")  #get login page
+#identify the login page  text box and enter the value 
+driver.find_element_by_id("email").clear() #clear the input
+driver.find_element_by_id("email").send_keys("nisha") #send value for email
 driver.find_element_by_id("password").clear()
-driver.find_element_by_id("password").send_keys("nisha")
+driver.find_element_by_id("password").send_keys("nisha") #send value for password 
 driver.find_element_by_id("btnsubmit").click()
-time.sleep(3)  
+
+time.sleep(3)  #sleep time for page loading
 time.sleep(3)
 
 #creating project 
-driver.get("http://localhost:4200/create")
+driver.get("http://localhost:4200/create")  #get project create page
 driver.find_element_by_name("projectname").clear()
-driver.find_element_by_name("projectname").send_keys("autotest")
+driver.find_element_by_name("projectname").send_keys("autotest1")
 
 driver.find_element_by_name("description").clear()
 driver.find_element_by_name("description").send_keys("auto discription test")
 
 driver.find_element_by_name("datasetname").clear()
-driver.find_element_by_name("datasetname").send_keys("autodatasetname")
+driver.find_element_by_name("datasetname").send_keys("autodatasetname1")
+
 # files = '../ingest/dataset/CarPrice_Assignment.csv'
 # file = {'inputfile': open(files, 'rb')}
-driver.find_element_by_name("file").clear()
+driver.find_element_by_name("file").clear() 
 driver.find_element_by_xpath("//input[@type='file']").send_keys("C:\\Users\\nbarad\\Desktop\\selenium_mlaas\\end-to-end-p1\\MS\\mlaas\\CarPrice_Assignment.csv")
-driver.find_element_by_id("createprj").click()
+driver.find_element_by_id("btnsubmit").click()
+
+time.sleep(3) 
+
+#get Data Detail Page
+driver.find_element_by_id("icon1").click()
+
+
 time.sleep(3)  
 time.sleep(3)
 
