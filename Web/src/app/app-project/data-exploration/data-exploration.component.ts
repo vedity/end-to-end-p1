edcf4a0ce38c672264d147721080faa4232e07c7
kmpatel2019 <karishma.patel@infosenseglobal.com>
2021-02-25 20:57:45 +0530

    issue fixing activity timeline and schema mapping

--- Web/src/app/app-project/data-exploration/data-exploration.component.ts
+++ Web/src/app/app-project/data-exploration/data-exploration.component.ts
@@ -93,6 +93,7 @@ export class DataExplorationComponent implements OnInit {
   columnlabelChart: any;
   columnlabelChartexpand: any;
   boxplotChartexpand: any;
+  BoxplotOptions:any;
   animation = "progress-dark";
   theme = {
     'border-radius': '5px',
@@ -106,6 +107,8 @@ export class DataExplorationComponent implements OnInit {
   displayleftoutliers: any = false;
 
   ngOnInit(): void {
+
+    
     this.dtOptions = {
       paging: false,
       ordering: false,
@@ -173,75 +176,89 @@ export class DataExplorationComponent implements OnInit {
       }
 
     };
-    // this.columnlabelChart = {
-    //   chart: {
-    //     width: '100%',
-    //     type: 'bar',
-    //     offsetX: 0,
-    //     offsetY: -26,
-    //     toolbar: {
-    //       show: false
-    //     },
-    //   },
-    //   plotOptions: {
-    //     bar: {
-    //       distributed: true
-    //     }
-    //   },
-    //   grid: {
-    //     xaxis: {
-    //       lines: {
-    //         show: false
-    //       }
-    //     },
-    //     yaxis: {
-    //       lines: {
-    //         show: false
-    //       }
-    //     },
-    //     padding: {
-    //       left: 0,
-    //       right: 0,
-    //       top: 0,
-    //       bottom: 0
-    //     },
-    //   },
-    //   colors: ['#34c38f'],
-    //   dataLabels: {
-    //     enabled: false
-    //   },
-    //   yaxis: {
-    //     axisBorder: {
-    //       show: false
-    //     },
-    //     axisTicks: {
-    //       show: false,
-    //     },
-    //     labels: {
-    //       show: false,
-    //       formatter: (val) => {
-    //         return val;
-    //       }
-    //     }
-    //   },
-    //   xaxis: {
-    //     axisBorder: {
-    //       show: false
-    //     },
-    //     axisTicks: {
-    //       show: false,
-    //     },
-    //     labels: {
-    //       show: false,
-    //       formatter: (val) => {
-    //         return val;
-    //       }
-    //     }
-    //   },
-    // };
 
-
-    this.getExplorationData(this.dataset_id,this.schema_id);
+    this.BoxplotOptions = {
+      series: [
+        {
+          name: 'box',
+          type: 'boxPlot',
+          data: [
+            {
+              x: new Date('2017-01-01').getTime(),
+              y: [54, 66, 69, 75, 88]
+            },
+            {
+              x: new Date('2018-01-01').getTime(),
+              y: [43, 65, 69, 76, 81]
+            },
+            {
+              x: new Date('2019-01-01').getTime(),
+              y: [31, 39, 45, 51, 59]
+            },
+            {
+              x: new Date('2020-01-01').getTime(),
+              y: [39, 46, 55, 65, 71]
+            },
+            {
+              x: new Date('2021-01-01').getTime(),
+              y: [29, 31, 35, 39, 44]
+            }
+          ]
+        },
+        {
+          name: 'outliers',
+          type: 'scatter',
+          data: [
+            {
+              x: new Date('2017-01-01').getTime(),
+              y: 32
+            },
+            {
+              x: new Date('2018-01-01').getTime(),
+              y: 25
+            },
+            {
+              x: new Date('2019-01-01').getTime(),
+              y: 64
+            },
+            {
+              x: new Date('2020-01-01').getTime(),
+              y: 27
+            },
+            {
+              x: new Date('2020-01-01').getTime(),
+              y: 78
+            },
+            {
+              x: new Date('2021-01-01').getTime(),
+              y: 15
+            }
+          ]
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "boxPlot"
+      },
+      colors: ['#008FFB', '#FEB019'],
+title: {
+  text: 'BoxPlot - Scatter Chart',
+  align: 'left'
+},
+xaxis: {
+  type: 'datetime',
+  tooltip: {
+    formatter: function(val) {
+      return new Date(val).getFullYear()
+    }
+  }
+},
+tooltip: {
+  shared: false,
+  intersect: true
+}
+    };
+      this.getExplorationData(this.dataset_id,this.schema_id);
   }
 
   getExplorationData(datasetid,schemaid) {
