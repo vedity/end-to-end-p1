872c95f08f7999048befab8ae91676c5adddbbd5
= <=>
2021-03-10 17:37:20 +0530

    Added Label Encoding SQL

--- MS/mlaas/preprocess/utils/Transformation/categorical_encoding.py
+++ MS/mlaas/preprocess/utils/Transformation/categorical_encoding.py
@@ -5,18 +5,28 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 
 class EncodeClass:
-    '''
-        Encoding Related Functions.
-    '''
     
-    def label_encoding(self, series):
+    # def label_encoding(self, series):
 
-        labelencoder = LabelEncoder()
+    #     labelencoder = LabelEncoder()
 
-        series = labelencoder.fit_transform(series)
+    #     series = labelencoder.fit_transform(series)
         
-        return series
-    
+    #     return series
+
+    def label_encoding(self,DBObject,connection,column_list, table_name,col_lst):
+        '''
+        Encoding Related Functions.
+        '''
+        try:
+            sql_command = "update {table_name} m SET '{column_list[1]}' = WANT_THIS from (SELECT  index, dense_rank() OVER ( ORDER BY '{column_list[1]}') AS WANT_THIS FROM {table_name}) s where m.'{column_list[0]}' = s.{column_list[0]};"
+            logging.info("label_encoding logging"+str(sql_command))
+            label_df = DBObject.update_record(connection,sql_command)
+            return label_df
+
+        except Exception as exc:
+                return exc
+        
     def one_hot_encoding(self, series):
         
         df = pd.DataFrame(series)
