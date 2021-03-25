deafb0b646fc5230f539af610708462f95647066
kmpatel2019 <karishma.patel@infosenseglobal.com>
2021-03-25 20:54:26 +0530

    comparison modeling

--- Web/src/app/app-modeling/modeling-type/modeling-type.component.ts
+++ Web/src/app/app-modeling/modeling-type/modeling-type.component.ts
@@ -5,7 +5,7 @@ import { DataTableDirective } from 'angular-datatables';
 import { ToastrService } from 'ngx-toastr';
 //  import { manualmodeling } from './modeling.model';
 import { ModelingTypeApiService } from '../modeling-type.service';
-import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-modeling-type',
   templateUrl: './modeling-type.component.html',
@@ -15,15 +15,15 @@ export class ModelingTypeComponent implements OnInit {
   // mySwitch: boolean = false;
   // public data:manualmodeling;
   timePeriods = [
-    'Experiment 1','Experiment 2','Experiment 3','Experiment 4'
+    'Experiment 1', 'Experiment 2', 'Experiment 3', 'Experiment 4'
   ];
 
- 
-  isDisplayRunning=true;
-  model_type="Regression";
-  currentuser:any;
-  splitmethodselection="crossvalidation";
-  hyperparams='sklearn';
+
+  isDisplayRunning = true;
+  model_type = "Regression";
+  currentuser: any;
+  splitmethodselection = "crossvalidation";
+  hyperparams = 'sklearn';
   @ViewChild(DataTableDirective, { static: false })
   datatableElement: DataTableDirective;
   dtOptions: DataTables.Settings = {};
@@ -45,19 +45,19 @@ export class ModelingTypeComponent implements OnInit {
   public modelDescription: any;
   processclass: any = "stop";
   processInterval: any;
-  lineColumAreaChart:any;
+  lineColumAreaChart: any;
 
   @HostListener('window:resize', ['$event'])
-	onResize(event) {
+  onResize(event) {
     if (this.datatableElement) {
       this.datatableElement.dtInstance.then((dtInstance: DataTables.Api) => {
         dtInstance.columns.adjust();
       })
     }
-	}
-  
+  }
+
   ngOnInit(): void {
-   let projectdatamodel;
+    let projectdatamodel;
     this.dtOptions = {
       paging: false,
       ordering: false,
@@ -72,75 +72,75 @@ export class ModelingTypeComponent implements OnInit {
       localStorage.setItem("modeling", JSON.stringify(this.params));
     }
     else {
-      if(this.params.selectproject==false){
+      if (this.params.selectproject == false) {
         $(".openmodal").trigger('click');
       }
-    //this.modalService.open(projectdatamodel, { size: 'lg', windowClass: 'modal-holder', centered: true });
+      //this.modalService.open(projectdatamodel, { size: 'lg', windowClass: 'modal-holder', centered: true });
       this.params = localStorage.getItem("params");
       this.params = JSON.parse(this.params);
     }
 
     this.lineColumAreaChart = {
       chart: {
-          height: 200,
-          type: 'line',
-          stacked: false,
-          toolbar: {
-              show: false
-          }
+        height: 200,
+        type: 'line',
+        stacked: false,
+        toolbar: {
+          show: false
+        }
       },
       stroke: {
-          width: [2, 2, 4],
+        width: [2, 2, 4],
       },
       colors: ['#f46a6a', '#34c38f'],
       series: [{
-          name: 'Experiment 1',
-          data: [23, 11, 50, 70, 13, 56, 37, 78, 44, 22, 30]
+        name: 'Experiment 1',
+        data: [23, 11, 50, 70, 13, 56, 37, 78, 44, 22, 30]
       }, {
-          name: 'Experiment 2',
-          data: [23, 10, 22, 30, 13, 20, 31, 21, 40, 20, 32]
+        name: 'Experiment 2',
+        data: [23, 10, 22, 30, 13, 20, 31, 21, 40, 20, 32]
       }],
       fill: {
-          opacity: [0.85, 1],
-          gradient: {
-              inverseColors: false,
-              shade: 'light',
-              type: 'vertical',
-              opacityFrom: 0.85,
-              opacityTo: 0.55,
-              stops: [0, 100, 100, 100]
-          }
+        opacity: [0.85, 1],
+        gradient: {
+          inverseColors: false,
+          shade: 'light',
+          type: 'vertical',
+          opacityFrom: 0.85,
+          opacityTo: 0.55,
+          stops: [0, 100, 100, 100]
+        }
       },
       // tslint:disable-next-line: max-line-length
       labels: ['01/01/2003', '02/01/2003', '03/01/2003', '04/01/2003', '05/01/2003', '06/01/2003', '07/01/2003', '08/01/2003', '09/01/2003', '10/01/2003', '11/01/2003'],
       markers: {
-          size: [0,2]
+        size: [0, 2]
       },
       legend: {
-          offsetY: 5,
+        offsetY: 5,
       },
       xaxis: {
-          type: 'datetime',
+        type: 'datetime',
       },
       yaxis: {
-          title: {
-              text: 'Points',
-          },
+        title: {
+          text: 'Points',
+        },
       },
       tooltip: {
-          shared: true,
-          intersect: false,
-          y: {
-              formatter(y) {
-                  if (typeof y !== 'undefined') {
-                      return y.toFixed(0) + ' points';
-                  }
-                  return y;
-              }
+        shared: true,
+        intersect: false,
+        y: {
+          formatter(y) {
+            if (typeof y !== 'undefined') {
+              return y.toFixed(0) + ' points';
+            }
+            return y;
           }
+        }
       },
       grid: {
-          borderColor: '#f1f1f1'
+        borderColor: '#f1f1f1'
       }
     };
     this.currentuser = localStorage.getItem("currentUser")
@@ -149,13 +149,13 @@ export class ModelingTypeComponent implements OnInit {
     this.getDatasetInfo();
     this.getRunningExperimentList();
     this.getAllExperimentList();
-    
+
   }
 
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.timePeriods, event.previousIndex, event.currentIndex);
   }
-  
+
   compareIds = [];
   setCopmareIds(val, id) {
     // console.log(val, id);
@@ -183,10 +183,10 @@ export class ModelingTypeComponent implements OnInit {
       logs => this.successHandler(logs),
       error => this.errorHandler(error));
 
-  } 
-  
+  }
+
   getAlgorithmList() {
-    this.apiservice.getAlgorithmList(this.params.dataset_id, this.params.project_id,this.model_type).subscribe(
+    this.apiservice.getAlgorithmList(this.params.dataset_id, this.params.project_id, this.model_type).subscribe(
       logs => this.successAlgorithmListHandler(logs),
       error => this.errorHandler(error));
   }
@@ -202,20 +202,20 @@ export class ModelingTypeComponent implements OnInit {
     }
   }
 
-  selectedalgorithm:any;
-  selectedalgorithmname:any;
-  getHyperParams(event){
-    let val=event.target.value;
-    this.selectedalgorithm=val;
-    this.selectedalgorithmname=event.target.selectedOptions[0].innerText;
+  selectedalgorithm: any;
+  selectedalgorithmname: any;
+  getHyperParams(event) {
+    let val = event.target.value;
+    this.selectedalgorithm = val;
+    this.selectedalgorithmname = event.target.selectedOptions[0].innerText;
     console.log(this.selectedalgorithmname);
-    if(val!=""){
+    if (val != "") {
       this.apiservice.getHyperparamsList(val).subscribe(
         logs => this.successParamsListHandler(logs),
         error => this.errorHandler(error));
     }
     else
-    this.paramsList=null
+      this.paramsList = null
   }
 
   paramsList: any;
@@ -223,7 +223,7 @@ export class ModelingTypeComponent implements OnInit {
     if (data.status_code == "200") {
       this.paramsList = data.response;
       console.log(this.paramsList);
-      
+
     }
     else {
       this.errorHandler(data);
@@ -231,11 +231,12 @@ export class ModelingTypeComponent implements OnInit {
   }
   getRunningExperimentList() {
     this.apiservice.showrunningexperimentslist(this.params.project_id).subscribe(
-      logs => this.runningexpListsuccessHandler(logs),
-      error => this.errorHandler(error));
+      logs => this.runningexpListsuccessHandler(logs)
+      // error => this.errorHandler(error)
+    );
   }
 
-runningExpList: any = [];
+  runningExpList: any = [];
   runningexpListsuccessHandler(data) {
     if (data.status_code == "200") {
       this.runningExpList = data.response;
@@ -248,14 +249,15 @@ runningExpList: any = [];
 
   getAllExperimentList() {
     this.apiservice.showallexperimentslist(this.params.project_id).subscribe(
-      logs => this.allexpListsuccessHandler(logs),
-      error => this.errorHandler(error));
+      logs => this.allexpListsuccessHandler(logs)
+      // error => this.errorHandler(error)
+    );
   }
 
   allExpList: any = [];
   allexpListsuccessHandler(data) {
     if (data.status_code == "200") {
-      this.allExpList=[];
+      this.allExpList = [];
       this.allExpList = data.response;
       this.contentloaded = true;
     }
@@ -273,46 +275,46 @@ runningExpList: any = [];
     let currentuser = localStorage.getItem("currentUser")
     let username = JSON.parse(currentuser).username;
     let obj;
-    if(this.model_mode=="Auto"){
+    if (this.model_mode == "Auto") {
       obj = {
         user_name: username,
-        dataset_id:this.params.dataset_id,
+        dataset_id: this.params.dataset_id,
         project_id: this.params.project_id,
         model_mode: this.model_mode,
-        model_type:this.model_type,
+        model_type: this.model_type,
         experiment_name: this.params.experiment_name,
         experiment_desc: this.params.experiment_desc,
-        model_id:0,
-        model_name:undefined
+        model_id: 0,
+        model_name: undefined
       }
     }
-    else{
+    else {
       obj = {
         user_name: username,
-        dataset_id:this.params.dataset_id,
+        dataset_id: this.params.dataset_id,
         project_id: this.params.project_id,
         model_mode: this.model_mode,
-        model_type:this.model_type,
+        model_type: this.model_type,
         experiment_name: this.params.experiment_name,
         experiment_desc: this.params.experiment_desc,
-        model_id:this.responsearray["model_id"],
-        model_name:this.responsearray["model_name"],
-        hyperparameters:this.responsearray["hyperparameters"]
+        model_id: this.responsearray["model_id"],
+        model_name: this.responsearray["model_name"],
+        hyperparameters: this.responsearray["hyperparameters"]
       }
       this.modalService.dismissAll();
     }
-    
+
     console.log(obj);
-    
+
     this.apiservice.startModeling(obj).subscribe(
-      logs=>this.startsuccessHandler(logs),
-      error=>this.errorHandler(error)
+      logs => this.startsuccessHandler(logs),
+      error => this.errorHandler(error)
     )
 
   }
 
-  changeDisplay(){
-    this.isDisplayRunning=!this.isDisplayRunning
+  changeDisplay() {
+    this.isDisplayRunning = !this.isDisplayRunning
   }
 
   stopModel() {
@@ -322,21 +324,21 @@ runningExpList: any = [];
     }
   }
 
-  checkstatus(){
+  checkstatus() {
     let currentuser = localStorage.getItem("currentUser")
     let username = JSON.parse(currentuser).username;
-    this.apiservice.checkmodelstatus(this.params.project_id,this.experiment_name,this.params.dataset_id,username).subscribe(
-      logs=>this.statusSuccessHandler(logs),
-      error=>this.errorHandler(error)
+    this.apiservice.checkmodelstatus(this.params.project_id, this.experiment_name, this.params.dataset_id, username).subscribe(
+      logs => this.statusSuccessHandler(logs)
+      // error=>this.errorHandler(error)
     )
   }
 
-  statusSuccessHandler(data){
+  statusSuccessHandler(data) {
     if (data.status_code == "200") {
-      if(data.response=="failed" || data.response=="success")
-      this.stopModel();
+      if (data.response == "failed" || data.response == "success")
+        this.stopModel();
     }
-    else{
+    else {
       this.errorHandler(data);
     }
   }
@@ -374,56 +376,55 @@ runningExpList: any = [];
   }
 
   modeltitle: any;
-  current_experiment_id:any;
-  current_model_type:any;
+  current_experiment_id: any;
+  current_model_type: any;
   extraLarge(exlargeModal: any, obj) {
     this.modeltitle = obj.experiment_name;
-    this.current_experiment_id=obj.experiment_id;
-    this.current_model_type=obj.model_type;
+    this.current_experiment_id = obj.experiment_id;
+    this.current_model_type = obj.model_type;
     this.modalService.open(exlargeModal, { size: 'xl', windowClass: 'modal-holder', centered: true });
   };
 
-  checkexperimentname(event)
-{
-  var val=event.target.value;
-  if(val!=""){
-    this.apiservice.checkexperimentname(val,this.params.project_id).subscribe(
-      logs=>this.checksuccessHandler(logs,event.target),
-      error=>this.errorHandler(error)
-    )
+  checkexperimentname(event) {
+    var val = event.target.value;
+    if (val != "") {
+      this.apiservice.checkexperimentname(val, this.params.project_id).subscribe(
+        logs => this.checksuccessHandler(logs, event.target),
+        error => this.errorHandler(error)
+      )
+    }
+    else
+      this.checkuniuqename = false;
   }
-  else
-  this.checkuniuqename = false;
-}
 
-checkuniuqename=true;
-checksuccessHandler(data,target){
-  if (data.status_code == '200') {
-    this.checkuniuqename = true;
-    target.className = target.className.replace("ng-invalid", " ");
-    target.className = target.className + " ng-valid";
+  checkuniuqename = true;
+  checksuccessHandler(data, target) {
+    if (data.status_code == '200') {
+      this.checkuniuqename = true;
+      target.className = target.className.replace("ng-invalid", " ");
+      target.className = target.className + " ng-valid";
+    }
+    else {
+      this.checkuniuqename = false;
+      target.className = target.className.replace("ng-valid", " ");
+      target.className = target.className + " ng-invalid";
+    }
   }
-  else {
-    this.checkuniuqename = false;
-    target.className = target.className.replace("ng-valid", " ");
-    target.className = target.className + " ng-invalid";
-  }
-}
 
 
   smallModal(modelingmodal: any) {
-   this.params.experiment_name=''
-   this.params.experiment_desc=''
+    this.params.experiment_name = ''
+    this.params.experiment_desc = ''
     this.modalService.open(modelingmodal, { size: 'md', windowClass: 'modal-holder', centered: true });
   }
 
-  model_mode='Auto';
+  model_mode = 'Auto';
   contentid = 0;
   LargeModal(largeModal: any, val) {
-  this.model_mode='Auto';
+    this.model_mode = 'Auto';
 
     if (val) {
-      this.model_mode='Manual';
+      this.model_mode = 'Manual';
 
       this.contentid = 1;
       this.getAlgorithmList();
@@ -431,12 +432,32 @@ checksuccessHandler(data,target){
     }
   };
 
-  compareLarge (compareModal: any) 
-  {
-    this.modalService.open(compareModal, { size: 'xl',windowClass:'modal-holder', centered: true });
+  compareExperiment(compareModal: any) {
+    console.log(this.compareIds);
+
+    this.apiservice.compareExperiment("[" + this.compareIds + "]").subscribe(
+      logs => this.manageCompareExperiment(logs, compareModal),
+      error => this.errorHandler(error)
+    )
+    // this.modalService.open(compareModal, { size: 'xl',windowClass:'modal-holder', centered: true });
+
   };
 
-  ProjectData(projectModal:any){
+  comparedata:any;
+  manageCompareExperiment(data, modal) {
+    if (data.status_code == "200") {
+      this.comparedata=data.response;
+      console.log(this.comparedata);
+      
+      this.modalService.open(modal, { size: 'xl', windowClass: 'modal-holder', centered: true });
+
+    }
+    else {
+
+    }
+  }
+
+  ProjectData(projectModal: any) {
     this.modalService.open(projectModal, { size: 'lg', windowClass: 'modal-holder', centered: true });
   }
 
@@ -444,56 +465,55 @@ checksuccessHandler(data,target){
     this.contentid = id;
   };
 
-  
-  responsearray={};
-  nextValidate(){
-    var errorflag=false;
-    var hyperparameters={};
-    if(this.selectedalgorithm!=""){
+
+  responsearray = {};
+  nextValidate() {
+    var errorflag = false;
+    var hyperparameters = {};
+    if (this.selectedalgorithm != "") {
       $(".hyperparamsinput").removeClass("errorinput")
-      if(this.paramsList.length>0){
-        var hyperparameters={};
-          this.paramsList.forEach(element => {
-            let val;
-            if(element.display_type==""){
-              val = $("#txt_"+element.param_name).val();
+      if (this.paramsList.length > 0) {
+        var hyperparameters = {};
+        this.paramsList.forEach(element => {
+          let val;
+          if (element.display_type == "") {
+            val = $("#txt_" + element.param_name).val();
+          }
+          if (element.display_type == "validation") {
+            val = $("#txt_" + element.param_name).val();
+            var valid = element.param_value;
+            if (val >= valid[0] && val <= valid[1]) {
             }
-            if(element.display_type=="validation"){
-              val = $("#txt_"+element.param_name).val();
-              var valid=element.param_value;
-              if(val>=valid[0] && val<=valid[1]){
-              }
-              else{
-                errorflag=true;
-                $("#txt_"+element.param_name).addClass("errorinput")
-              }
+            else {
+              errorflag = true;
+              $("#txt_" + element.param_name).addClass("errorinput")
             }
-            if(element.display_type=="dropdown"){
-              val = $("#txt_"+element.param_name).val();
-            }
-            if(val!=""){
-            element.value=val;
-            hyperparameters[element.param_name]=val;
-            }
-            else
-            {
-            $("#txt_"+element.param_name).addClass("errorinput")
-            errorflag=true;
-            }
-          });
-        if(errorflag)
-      this.toaster.error("Please enter all valid input",'Error');
-          
+          }
+          if (element.display_type == "dropdown") {
+            val = $("#txt_" + element.param_name).val();
+          }
+          if (val != "") {
+            element.value = val;
+            hyperparameters[element.param_name] = val;
+          }
+          else {
+            $("#txt_" + element.param_name).addClass("errorinput")
+            errorflag = true;
+          }
+        });
+        if (errorflag)
+          this.toaster.error("Please enter all valid input", 'Error');
+
       }
-      this.responsearray["model_id"]=this.selectedalgorithm;
-      this.responsearray["model_name"]=this.selectedalgorithmname;
-      this.responsearray["hyperparameters"]=hyperparameters;
+      this.responsearray["model_id"] = this.selectedalgorithm;
+      this.responsearray["model_name"] = this.selectedalgorithmname;
+      this.responsearray["hyperparameters"] = hyperparameters;
     }
-    else{
-      this.toaster.error("Please select model",'Error');
-      errorflag=true
+    else {
+      this.toaster.error("Please select model", 'Error');
+      errorflag = true
     }
-    if(!errorflag){
+    if (!errorflag) {
       this.modalService.dismissAll();
       $("#createExperiment").trigger('click');
     }
